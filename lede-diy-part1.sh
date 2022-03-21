9b160c282d1b8651ccdad13c51aeef1805b31a27
fjcanyue <fjcanyue@163.com>
2022-03-21 18:04:06 +0800

    update

---
+++ lede-diy-part1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git luamaxminddb https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
+echo 'src-git vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
